09157a8b28fbdd7d0fd843c0936477c185dd31ca
Shweta Bhagat <bhagatshweta0216@gmail.com>
2022-01-30 00:15:46 +0530

    Leetcode Solutions

---
+++ Leetcode_Solutions/1791.cpp
@@ -0,0 +1,25 @@
+//1791. Find Center of Star Graph
+
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges)
+    {
+        int ans=0;
+        int n=edges.size();
+        vector<int>v(n+2,0);
+        for(auto i:edges)
+        {
+            v[i[0]]++;
+            v[i[1]]++;
+        }
+        for(int i=0;i<v.size();i++)
+        {
+            if(v[i]==n)
+                ans=i;
+        }
+        return ans;
+    }
+};
+
+
