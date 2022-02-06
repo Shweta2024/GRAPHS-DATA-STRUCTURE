41d848fa15772e3c8435694100b0271860cc770d
Shweta Bhagat <bhagatshweta0216@gmail.com>
2022-02-06 21:25:19 +0530

    The time when the network becomes idle

--- Leetcode_Solutions/2039.cpp
+++ Leetcode_Solutions/2039.cpp
@@ -1,8 +1,19 @@
+/*
+2039. The Time When the Network Becomes Idle
+
+https://leetcode.com/problems/the-time-when-the-network-becomes-idle/
+
+
+*/
+
+
 class Solution {
 public:
     int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience) {
         int ans = 0;
         int n = patience.size(); //total no. of nodes
+
+        //adjacency list->{time,adjacent node}
         vector<pair<int,int>>adj[n]; //0-based indexing
         
         //formulate the adjacency list
@@ -11,24 +22,28 @@ public:
             adj[i[0]].push_back({1,i[1]});
             adj[i[1]].push_back({1,i[0]});
         }
-        
+
+        //creating a min heap
+        //pq -> {differnce of time,adjacent node}
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
-        vector<int>dist(n,INT_MAX);
-        dist[0]=0;
+        vector<int>dist(n,INT_MAX);//to store the minimum time taken by each node from the source node
+        dist[0]=0;//since the time taken by source to send message to itself is ALWAYS 0 
         pq.push({0,0}); //src node dist = 0
+        
+        //traverse pq untill it gets empty
         while(!pq.empty()) 
         {
-            int prev = pq.top().second;
-            int prev_dist = pq.top().first;
-            pq.pop();
-            for(auto i:adj[prev])
+            int prev = pq.top().second;//extract prev
+            int prev_dist = pq.top().first;//extract  the prev_dist
+            pq.pop();//after extracting,pop them from pq
+            for(auto i:adj[prev]) //traverse though all the adjacent nodes of prev
             {
-                int next = i.second;
-                int next_dist = i.first;
-                if(dist[next] > prev_dist + next_dist)
+                int next = i.second; //extract next
+                int next_dist = i.first;//extract next_distance
+                if(dist[next] > prev_dist + next_dist)//check if the time taken by next is greater than the new time,then update it
                 {
                     dist[next] = prev_dist + next_dist;
-                    pq.push({dist[next],next});
+                    pq.push({dist[next],next});//push the next node with the updated time value
                 }
             }
             
